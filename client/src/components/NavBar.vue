fe3c492dc10f79d8c4d479f1ef157c8870b7eed6
Quentin Oschatz <quentin@cliffhouse.de>
2018-04-19 12:44:44 +0200

    feat$(Vue): Add basic NavBar
    Add NavBar with vue-material

---
+++ client/src/components/NavBar.vue
@@ -0,0 +1,44 @@
+<template>
+  <div id="navbar">
+
+    <!-- Toolbar -->
+    <md-toolbar>
+
+      <!-- Title -->
+      <h3 class="md-title" style="flex: 1">Title</h3>
+
+      <!-- Dropdown -->
+      <md-menu>
+        <md-button class="md-icon-button" md-menu-trigger>
+          <md-icon>more_vert</md-icon>
+        </md-button>
+        <!-- Dropdown content -->
+
+        <md-menu-content>
+          <md-menu-item>
+            <router-link :to="{ name: '', params: {} }">Login</router-link>
+          </md-menu-item>
+        </md-menu-content>
+
+      </md-menu>
+
+    </md-toolbar>
+
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'navbar',
+  components: {
+
+  }
+}
+</script>
+
+<style scoped>
+  .md-toolbar {
+    margin-top: 16px;
+  }
+</style>
--- client/src/main.js
+++ client/src/main.js
@@ -1,6 +1,15 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+// Vue Material
+import VueMaterial from 'vue-material'
+import 'vue-material/dist/vue-material.min.css'
+// Theme
+import 'vue-material/dist/theme/black-green-dark.css' // This line here
+
+// Use Vue Material
+Vue.use(VueMaterial)
+
 Vue.config.productionTip = false
 
 new Vue({
