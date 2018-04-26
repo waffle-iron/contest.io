4a41b90d4d2c193429fea8a80539c83de4f35b5e
David Wu <david10608@gmail.com>
2018-04-26 17:45:50 +0200

    feat$(Flask, database, models): fix task insertion, add contest insertion

--- server/database/models.py
+++ server/database/models.py
@@ -1,16 +1,31 @@
 import sqlite3 as sql
 import json
+import secrets
+import datetime
+
+def insert_task(name, tags, url):
+    with sql.connect("server/database/database.db") as dbcon:
+        cur = dbcon.cursor()
+        stringified_tags = json.dumps(tags)
+        cur.execute(
+            "INSERT INTO Task (taskname, tasktags, codeforces_url) VALUES (?,?,?)",
+            (name, stringified_tags, url)
+        )
+        cur.close()
+        dbcon.commit()
 
 
-def insert_task(name, tags):
+def insert_contest(name, date_start, date_end, visible, contestgroup):
     with sql.connect("database.db") as dbcon:
         cur = dbcon.cursor()
+        random_code = secrets.token_hex(16)
+        date_start = datetime.datetime(*date_start)
+        date_end = datetime.datetime(*date_end)
         cur.execute(
-            "INSERT INTO Task (taskname, tasktags) VALUES (?,?)",
-            (name, json.dumps(tags))
+            "INSERT INTO Contest (contestcode, contestname, date_start, date_end, visible, contestgroup) VALUES (?,?,?,?,?,?)",
+            (random_code, name, date_start, date_end, visible, contestgroup)
         )
         dbcon.commit()
-        dbcon.close()
 
 
 def insert_user(name, usertype, oauth_token):
@@ -20,6 +35,4 @@ def insert_user(name, usertype, oauth_token):
             "INSERT INTO USER (username, usertype, oauth_token) VALUES (?,?,?)",
             (name, usertype, oauth_token)
         )
-		dbcon.commit()
-		dbcon.close()
-		
+        dbcon.commit()
