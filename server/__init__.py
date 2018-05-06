66ed3a98d0b289949a589f0bb6ae8a72d695ecfa
Felix Wu <mail@flxwu.com>
2018-05-06 10:00:32 +0200

    feat$(Flask, endpoints, tasks): also query with tags from db

--- server/__init__.py
+++ server/__init__.py
@@ -1,10 +1,12 @@
 from flask import Flask, render_template, jsonify, request, g, session, redirect, url_for
 from werkzeug.routing import BaseConverter
-import requests, json
+import requests
+import json
 from flask_github import GitHub
 import server.api.APIConnector as APIConnector
 import server.database.models as models
 import server.settings as settings
+
 
 class RegexConverter(BaseConverter):
     def __init__(self, url_map, *items):
@@ -13,8 +15,8 @@ class RegexConverter(BaseConverter):
 
 
 app = Flask(__name__,
-            static_folder = "../client/dist/static",
-            template_folder = "../client/dist")
+            static_folder="../client/dist/static",
+            template_folder="../client/dist")
 # Flask app config
 app.config['GITHUB_CLIENT_ID'] = settings.GITHUB_CLIENT_ID
 app.config['GITHUB_CLIENT_SECRET'] = settings.GITHUB_CLIENT_SECRET
@@ -30,31 +32,34 @@ github = Github(app)
 
 @app.route('/')
 def index():
-    
     if app.debug:
         return requests.get('http://localhost:3000/index.html').text
     return render_template("index.html")
+
 
 @app.route('/github-login')
 def auth_GithubLogin():
     return github.authorize()
 
+
+# TODO: Github Callback
 @app.route('/github-callback')
 @github.authorized_handler
 def auth_GithubCallback(auth_token):
     next_url = request.args.get('next') or url_for('index')
-    if oauth_token is None:
-        flash("Authorization failed.")
-        return redirect(next_url)
+    # if oauth_token is None:
+    #     flash("Authorization failed.")
+    #     return redirect(next_url)
 
-    user = 
-    if user is None:
-        user = User(oauth_token)
-        db_session.add(user)
+    # user = models.select_user(params=())
+    # if user is None:
+    #     user = User(oauth_token)
+    #     db_session.add(user)
 
-    user.github_access_token = oauth_token
-    db_session.commit()
+    # user.github_access_token = oauth_token
+    # db_session.commit()
     return redirect(next_url)
+
 
 @app.route('/api/tasks', methods=['GET', 'POST'])
 def api_tasks():
@@ -63,15 +68,17 @@ def api_tasks():
 
         def queryparam_tags(x): return request.args.get(
             'tags') if request.args.get('tags') else None
-        
-        tasks_in_database = models.select_task()
+
+        if queryparam_tags(None) != None:
+            tasks_in_database = models.select_task(conditions=(
+                "tasktags LIKE '%{}%'".format(queryparam_tags(None))))
 
         if tasks_in_database is not None:
             returnJSON = tasks_in_database
         else:
             respond_rawdata = TasksEndpoint.get(tags=queryparam_tags(None))
             returnJSON = respond_rawdata
-        
+
         # @FRONT-END tasktags have to be parsed -> JSON.parse
         return json.dumps(returnJSON)
     else:
