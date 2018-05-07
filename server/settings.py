0d6082cd38794cf56c3bb920c54573cbc488d829
Felix Wu <mail@flxwu.com>
2018-05-07 20:26:20 +0200

    feat$(Flask): move database string constants (columns) to settings

--- server/settings.py
+++ server/settings.py
@@ -1,5 +1,5 @@
 from dotenv import load_dotenv
-from pathlib import Path 
+from pathlib import Path
 import os
 
 env_path = Path('..') / '.env'
@@ -7,3 +7,18 @@ load_dotenv(dotenv_path=env_path)
 
 GITHUB_CLIENT_ID = os.getenv('GITHUB_CLIENT_ID')
 GITHUB_CLIENT_SECRET = os.getenv('GITHUB_CLIENT_SECRET')
+
+ADMIN_USERTYPE = 'admin'
+NORMAL_USERTYPE = 'normal'
+
+
+class DB_COLUMNS:
+    USER_USERID = 'userid'
+    USER_USERNAME = 'username'
+    USER_USERTYPE = 'usertype'
+    USER_DATE_JOINED = 'date_joined'
+    USER_OAUTH_TOKEN = 'oauth_token'
+    TASK_TASKID = 'taskid'
+    TASK_TASKNAME = 'taskname'
+    TASK_TASKTAGS = 'tasktags'
+    TASK_CODEFORCES_URL = 'codeforces_url'
